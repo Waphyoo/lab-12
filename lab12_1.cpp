a56bcbbb2a5115110daa8a3016e318d9e804ea8f
n <n@gmail.com>
2023-01-06 17:19:45 +0700

    okok

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,31 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+
+int main(){
+    
+    int i;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    i=rand()%9;
+    string g;
+    if(i==0)g="A";
+    else if(i==1)g="B+";
+    else if(i==2)g="B";
+    else if(i==3)g="C+";
+    else if(i==4)g="C";
+    else if(i==5)g="D+";
+    else if(i==6)g="D";
+    else if(i==7)g="F";
+    else if(i==8)g="W";
+
+cout<<"You will get "<<g<<" in this 261102.";
+}
